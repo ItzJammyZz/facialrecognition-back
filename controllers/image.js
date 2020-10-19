6dea9aeff30da3e62c7d617734038a2926bb7aca
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2020-10-19 17:29:56 +0100

    Heroku commit

---
+++ controllers/image.js
@@ -0,0 +1,45 @@
+
+const Clarifai = require ('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: '887893022f414e7da0d611e0e0d5eaf4'
+  });
+
+  const handleAPICall = (req, res) => {
+  app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+  }
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+  
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
+    // let found = false;
+    // database.users.forEach(user => {
+    //   if (user.id === id) {
+    //     found = true;
+    //     user.entries++;
+    //     return res.json(user.entries);
+    //   }
+    // });
+    // if (!found) {
+    //   res.status(400).json("user entries not found");
+    // }
+  }
+
+  module.exports = {
+      handleImage,
+      handleAPICall
+  }
