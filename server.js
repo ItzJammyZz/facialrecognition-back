cd49c65e1b3af2abb57726a1949fbc1a344f6dc1
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2020-10-19 19:18:23 +0100

    changing the syntax in const db

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
 const db = knex({
-  client: 'pg',
-  connection: {
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-  }
+  client: "pg",
+  connection: process.env.DATABASE_URL
 });
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+//   }
+// });
 
 
 const app = express();
