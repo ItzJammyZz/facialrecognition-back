a277e99e4af2d04c99a4dfbe8ed14b11c33b8fd4
ItzJammyZz <jammy-anwar@hotmail.co.uk>
2023-10-27 19:35:05 +0100

    cl4

--- controllers/register.js
+++ controllers/register.js
@@ -38,7 +38,7 @@ const  handleRegister = (req, res, db, bcrypt) => {
   
     .catch(err => {
       console.log("Error during registration:", err);
-      res.status(400).json('Unable to register');
+      res.status(400).json('Unable to register nothing');
   });
   }
 
